d7d3c3a391bc9ee2911d9d9a8260d41b41782aeb
FHOERR <1677153755@qq.com>
2023-06-14 17:48:00 +0800

    更新注释

---
+++ microc/test/noteTest.c
@@ -0,0 +1,19 @@
+void main(int n)
+{
+  // print 1;
+  // print 2;
+  print 3;
+  /*
+   print 4;
+   */
+  int a;
+  a = 1;
+  for(a;a < 3;a = a + 1){
+      print 4 + a;
+      // print -1;
+      /*
+       print -2;
+       */
+  }
+  print -3;
+}
